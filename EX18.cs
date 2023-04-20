f8ea692034d4ba4ab4b3ba799c46386cf56c2d2a
DiogoYudi <130068159+DiogoYudi@users.noreply.github.com>
2023-04-20 17:12:57 -0300

    Update and rename Program.cs to EX18.cs

--- EX18.cs
+++ EX18.cs
@@ -1,4 +1,14 @@
-﻿void Cadastre(ref tp_no l)
+//18) Faça um programa que utilize lista encadeada e que tenha as opções a seguir. O nó deve conter os atributos: nome, idade, whats e prox.
+	
+//Incluir conforme apresentado em aulas
+
+//Para alterar, consulte pelo nome. Se encontrar, exiba os valores atuais e permita a alteração. Caso não encontre, exiba mensagem de não encontrado.
+
+//Para excluir, procure pelo nome. Se encontrar, exiba os valores atuais e permita a exclusão. Caso não encontre, exiba mensagem de não encontrado.
+
+//Na opção exibir, exiba todos os registros.
+
+void Cadastre(ref tp_no l)
 {
     Console.Write("Nome: ");
     string nome = Console.ReadLine();
